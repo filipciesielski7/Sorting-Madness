e76c456a45d7a98942b06ff4aa162c30563f4180
filipciesielski7 <filipcies@gmail.com>
2021-12-13 17:06:45 +0100

    Added insertion sort for objects

--- src/main/java/pl/put/poznan/sortingMadness/logic/InsertionSortStrategy.java
+++ src/main/java/pl/put/poznan/sortingMadness/logic/InsertionSortStrategy.java
@@ -46,7 +46,7 @@ public class InsertionSortStrategy implements SortStrategy {
 
         String temp = "";
 
-        for(int i=0;i<f;i++) {
+        for(int i = 0; i < f; i++) {
             for (int j = i + 1; j < f; j++) {
                 if (arr[i].compareToIgnoreCase(arr[j]) > 0) {
                     temp = arr[i];
@@ -59,10 +59,49 @@ public class InsertionSortStrategy implements SortStrategy {
         return arr;
     }
 
+    /**
+     * Objects array sorting.
+     *
+     * @param arr
+     *            is the name of the data input objects array.
+     * @param sorting_attribute
+     *            is the name of the sorting based on attribute.
+     * @return sorted objects array.
+     */
     @Override
-    public JSONObject[] sortingObject(JSONObject[] data_array, String sorting_attribute) {
-        return data_array;
+    public JSONObject[] sortingObject(JSONObject[] arr, String sorting_attribute) {
+        if(arr[0].get(sorting_attribute) instanceof Integer){
+            int n = arr.length;
+            for (int i = 1; i < n; ++i) {
+                JSONObject key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j].getInt(sorting_attribute) > key.getInt(sorting_attribute)) {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+
+            return arr;
+        }
+        else if(arr[0].get(sorting_attribute) instanceof String){
+            int f = arr.length;
+
+            JSONObject temp;
+
+            for(int i = 0; i < f; i++) {
+                for (int j = i + 1; j < f; j++) {
+                    if (arr[i].getString(sorting_attribute).compareToIgnoreCase(arr[j].getString(sorting_attribute)) > 0) {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            return arr;
+        }
+        return arr;
     }
-
-
 }
