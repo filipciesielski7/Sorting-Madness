1cded45e99d6dcb2c57442ae3fb77376a233dfe7
filipciesielski7 <filipcies@gmail.com>
2021-12-13 18:30:07 +0100

    Added merge sort for objects

--- src/main/java/pl/put/poznan/sortingMadness/logic/MergeSortStrategy.java
+++ src/main/java/pl/put/poznan/sortingMadness/logic/MergeSortStrategy.java
@@ -17,8 +17,78 @@ public class MergeSortStrategy implements SortStrategy {
     }
 
     @Override
-    public JSONObject[] sortingObject(JSONObject[] data_array, String sorting_attribute) {
-        return data_array;
+    public JSONObject[] sortingObject(JSONObject[] arr, String sorting_attribute) {
+        if(arr[0].get(sorting_attribute) instanceof Integer) {
+            sortObject(arr, 0, arr.length-1, sorting_attribute);
+            return arr;
+        }
+        else if(arr[0].get(sorting_attribute) instanceof String){
+            sortObjectText(arr, sorting_attribute);
+            return arr;
+        }
+        return arr;
+    }
+
+    void sort(int arr[], int l, int r)
+    {
+        if (l < r) {
+            int m =l+ (r-l)/2;
+
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            merge(arr, l, m, r);
+        }
+    }
+
+    void sortText(String[] names) {
+        if (names.length >= 2) {
+            String[] left = new String[names.length / 2];
+            String[] right = new String[names.length - names.length / 2];
+
+            for (int i = 0; i < left.length; i++) {
+                left[i] = names[i];
+            }
+
+            for (int i = 0; i < right.length; i++) {
+                right[i] = names[i + names.length / 2];
+            }
+
+            sortText(left);
+            sortText(right);
+            mergeText(names, left, right);
+        }
+    }
+
+    void sortObject(JSONObject[] arr, int l, int r, String sorting_attribute)
+    {
+        if (l < r) {
+            int m = l + (r-l) /2;
+
+            sortObject(arr, l, m, sorting_attribute);
+            sortObject(arr, m + 1, r, sorting_attribute);
+
+            mergeObject(arr, l, m, r, sorting_attribute);
+        }
+    }
+
+    void sortObjectText(JSONObject[] arr, String sorting_attribute) {
+        if (arr.length >= 2) {
+            JSONObject[] left = new JSONObject[arr.length / 2];
+            JSONObject[] right = new JSONObject[arr.length - arr.length / 2];
+
+            for (int i = 0; i < left.length; i++) {
+                left[i] = arr[i];
+            }
+
+            for (int i = 0; i < right.length; i++) {
+                right[i] = arr[i + arr.length / 2];
+            }
+
+            sortObjectText(left, sorting_attribute);
+            sortObjectText(right, sorting_attribute);
+            mergeObjectText(arr, left, right, sorting_attribute);
+        }
     }
 
     void merge(int arr[], int l, int m, int r)
@@ -62,18 +132,6 @@ public class MergeSortStrategy implements SortStrategy {
         }
     }
 
-    void sort(int arr[], int l, int r)
-    {
-        if (l < r) {
-            int m =l+ (r-l)/2;
-
-            sort(arr, l, m);
-            sort(arr, m + 1, r);
-
-            merge(arr, l, m, r);
-        }
-    }
-
     void mergeText(String[] names, String[] left, String[] right) {
         int a = 0;
         int b = 0;
@@ -88,22 +146,58 @@ public class MergeSortStrategy implements SortStrategy {
         }
     }
 
-    void sortText(String[] names) {
-        if (names.length >= 2) {
-            String[] left = new String[names.length / 2];
-            String[] right = new String[names.length - names.length / 2];
+    void mergeObject(JSONObject[] arr, int l, int m, int r, String sorting_attribute)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
 
-            for (int i = 0; i < left.length; i++) {
-                left[i] = names[i];
+        JSONObject L[] = new JSONObject[n1];
+        JSONObject R[] = new JSONObject[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i].getInt(sorting_attribute) <= R[j].getInt(sorting_attribute)) {
+                arr[k] = L[i];
+                i++;
             }
-
-            for (int i = 0; i < right.length; i++) {
-                right[i] = names[i + names.length / 2];
+            else {
+                arr[k] = R[j];
+                j++;
             }
+            k++;
+        }
 
-            sortText(left);
-            sortText(right);
-            mergeText(names, left, right);
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    void mergeObjectText(JSONObject[] arr, JSONObject[] left, JSONObject[] right, String sorting_attribute) {
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (b >= right.length || (a < left.length && left[a].getString(sorting_attribute).compareToIgnoreCase(right[b].getString(sorting_attribute)) < 0)) {
+                arr[i] = left[a];
+                a++;
+            } else {
+                arr[i] = right[b];
+                b++;
+            }
         }
     }
 }
