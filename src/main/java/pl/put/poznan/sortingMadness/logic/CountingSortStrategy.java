b0074cbc26e4e9dd6bde5cfb0ae6ca07cd65835a
filipciesielski7 <filipcies@gmail.com>
2021-12-13 17:52:53 +0100

    Added counting sort for objects

--- src/main/java/pl/put/poznan/sortingMadness/logic/CountingSortStrategy.java
+++ src/main/java/pl/put/poznan/sortingMadness/logic/CountingSortStrategy.java
@@ -73,7 +73,62 @@ public class CountingSortStrategy implements SortStrategy {
     }
 
     @Override
-    public JSONObject[] sortingObject(JSONObject[] data_array, String sorting_attribute) {
-        return data_array;
+    public JSONObject[] sortingObject(JSONObject[] arr, String sorting_attribute) {
+        if(arr[0].get(sorting_attribute) instanceof Integer) {
+            int max = arr[0].getInt(sorting_attribute);
+            int min = arr[0].getInt(sorting_attribute);
+
+            for(int i = 0; i < arr.length; i++){
+                if(arr[i].getInt(sorting_attribute) >= max){
+                    max = arr[i].getInt(sorting_attribute);
+                }
+                if(arr[i].getInt(sorting_attribute) <= min) {
+                    min = arr[i].getInt(sorting_attribute);
+                }
+            }
+
+            int range = max - min + 1;
+            int count[] = new int[range];
+            JSONObject output[] = new JSONObject[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                count[arr[i].getInt(sorting_attribute) - min]++;
+            }
+
+            for (int i = 1; i < count.length; i++) {
+                count[i] += count[i - 1];
+            }
+
+            for (int i = arr.length - 1; i >= 0; i--) {
+                output[count[arr[i].getInt(sorting_attribute) - min] - 1] = arr[i];
+                count[arr[i].getInt(sorting_attribute) - min]--;
+            }
+
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = output[i];
+            }
+
+            return arr;
+        }
+        else if(arr[0].get(sorting_attribute) instanceof String){
+            int n = arr.length;
+
+            JSONObject temp;
+
+            for (int j = 0; j < n - 1; j++)
+            {
+                for (int i = j + 1; i < n; i++)
+                {
+                    if (arr[j].getString(sorting_attribute).compareTo(arr[i].getString(sorting_attribute)) > 0)
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+
+            return arr;
+        }
+        return arr;
     }
 }
