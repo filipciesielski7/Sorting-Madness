2e706433d47848258b2310b1f8948f75e1928af9
filipciesielski7 <filipcies@gmail.com>
2021-12-13 17:07:51 +0100

    Modified controller paths

--- src/main/java/pl/put/poznan/sortingMadness/rest/SortingMadnessController.java
+++ src/main/java/pl/put/poznan/sortingMadness/rest/SortingMadnessController.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pl.put.poznan.sortingMadness.logic.SortingMadness;
+import pl.put.poznan.sortingMadness.logic.SortingNumberMadness;
 import pl.put.poznan.sortingMadness.logic.SortingObjectMadness;
 import pl.put.poznan.sortingMadness.logic.SortingTextMadness;
 
@@ -20,21 +20,20 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/{sorting_type}/")
+@RequestMapping("/")
 public class SortingMadnessController {
 
     private static final Logger logger = LoggerFactory.getLogger(SortingMadnessController.class);
 
     // example -> http://localhost:8080/insertion/?numbers=5,4,3,2,1,4,2,5,6
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/{sorting_type}/", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<Object> get(@PathVariable String sorting_type,
                               @RequestParam(value="numbers", defaultValue="") String[] numbers) {
 
         logger.debug(sorting_type);
         logger.debug(Arrays.toString(numbers));
 
-        SortingMadness sorter = new SortingMadness(sorting_type);
-        logger.debug(sorter.getSorting_type());
+        SortingNumberMadness sorter = new SortingNumberMadness(sorting_type);
 
         Instant start = Instant.now();
         int [] sorted_table = sorter.sort(numbers);
@@ -54,8 +53,8 @@ public class SortingMadnessController {
         return new ResponseEntity<>(sorter, HttpStatus.OK);
     }
 
-    // example -> http://localhost:8080/insertion/text/?strings=kota,ma,ala
-    @RequestMapping(value="/text/", method = RequestMethod.GET, produces = "application/json")
+    // example -> http://localhost:8080/text/insertion/?strings=kota,ma,ala
+    @RequestMapping(value="/text/{sorting_type}/", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<Object> getString(@PathVariable String sorting_type,
                                       @RequestParam(value="strings", defaultValue="") String[] strings) {
 
@@ -63,7 +62,6 @@ public class SortingMadnessController {
         logger.debug(Arrays.toString(strings));
 
         SortingTextMadness sorter = new SortingTextMadness(sorting_type);
-        logger.debug(sorter.getSorting_type());
 
         Instant start = Instant.now();
         String [] sorted_table = sorter.sort(strings);
@@ -83,14 +81,21 @@ public class SortingMadnessController {
         return new ResponseEntity<>(sorter, HttpStatus.OK);
     }
 
+    /*
+         example ->
+         http://localhost:8080/
+         {
+            "sortingType": "bubble",
+            "data": [5,4,3,2,1]
+         }
+     */
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity<Object> post(@RequestBody SortingInput json) {
 
         logger.debug(json.getSortingType());
         logger.debug(Arrays.toString(json.getData()));
 
-        // silly in api with db
-        SortingMadness sorter = new SortingMadness(json.getSortingType());
+        SortingNumberMadness sorter = new SortingNumberMadness(json.getSortingType());
 
         int [] sorted_table;
         Instant start = Instant.now();
@@ -111,13 +116,20 @@ public class SortingMadnessController {
         return new ResponseEntity<>(sorter, HttpStatus.OK);
     }
 
+    /*
+         example ->
+         http://localhost:8080/text/
+         {
+            "sortingType": "insertion",
+            "data": ["d", "e","c", "b", "a"]
+         }
+     */
     @RequestMapping(value = "/text/", method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity<Object> postText(@RequestBody SortingInput json) {
 
         logger.debug(json.getSortingType());
         logger.debug(Arrays.toString(json.getData()));
 
-        // silly in api with db
         SortingTextMadness sorter = new SortingTextMadness(json.getSortingType());
 
         String [] sorted_table;
@@ -139,6 +151,31 @@ public class SortingMadnessController {
         return new ResponseEntity<>(sorter, HttpStatus.OK);
     }
 
+    /*
+         example ->
+         http://localhost:8080/object/
+            {
+                "sortingType": "bubble",
+                    "data": [
+                {
+                    "a": 1,
+                        "b": 5,
+                        "c": "c"
+                },
+                {
+                    "a": 3,
+                        "b": 4,
+                        "c": "a"
+                },
+                {
+                    "a": 6,
+                        "b": 2,
+                        "c": "b"
+                }
+            ],
+                "sortingAttribute": "a"
+            }
+    */
     @RequestMapping(value = "/object/", method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity<Object> postObject(@RequestBody SortingInputObject json) {
 
@@ -146,11 +183,9 @@ public class SortingMadnessController {
         logger.debug(new Gson().toJson(json.getData()));
         JSONObject data_array = new JSONObject(json);
 
-        // silly in api with db
         SortingObjectMadness sorter = new SortingObjectMadness(data_array.get("sortingType").toString());
 
         JSONObject[] sorted_table;
-        Instant start = Instant.now();
 
         JSONObject[] arr = new JSONObject[((JSONArray) data_array.get("data")).length()];
         for (int i = 0; i < ((JSONArray) data_array.get("data")).length(); i++) {
@@ -158,6 +193,7 @@ public class SortingMadnessController {
             arr[i] = object;
         }
 
+        Instant start = Instant.now();
         sorted_table = sorter.sort(arr, data_array.get("sortingAttribute").toString());
 
         if (((JSONArray) data_array.get("data")).length() == 0) {
